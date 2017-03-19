5d5abee49655c71b522b7eb9ca9bdec420de14fe
najibu <ns.najibu@gmail.com>
2017-03-19 12:19:52 +0000

    added setup

---
+++ app.js
@@ -0,0 +1,51 @@
+// Requiring all dependencies 
+var express = require('express');
+var path = require('path');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var session = require('express-session');
+var dotenv = require('dotenv');
+
+// We are using the dotenv library to load our environmental variables from the .env file.
+dotenv.load();
+
+// Import routes
+var routes = require('./routes/index');
+
+// Instantiates the Express JS framework
+var app = express();
+
+// The .set method allows us to configure various options with the Express framework. Here we are setting our views directory as well as telling Express that our templating engine will be Jade.
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+// The .use method is similar to the .set method, where it allows us to set further configurations. The .use method also acts as a chain of events that will take place once a request hits our Node Js application. First we'll log the request data, parse any incoming data, and so on.
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(session({
+  // Here we are creating a unique session identifier
+  secret: 'shhhhhhhhh',
+  resave: true, 
+  saveUninitialized: true
+}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+// catch 404 and forward to error handler.
+app.use(function(req, res, next){
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// If our applicatione encounters an error, we'll display the error and stacktrace accordingly.
+app.use(function(err, req, res, next){
+  res.status(err.status || 500);
+  res.send(err);
+});
+
+
+// Finally, we'll choose to have our app listen on port 3000. This means that once we launch our app, we'll be able to navigate to localhost:3000 and see our app in action. 
+app.listen(3000);
