92a6e0cdc910d207116e53b634280da21fb036a4
najibu <ns.najibu@gmail.com>
2017-03-22 15:17:53 +0000

    Finished app

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-// Requiring all dependencies 
 var express = require('express');
 var path = require('path');
 var logger = require('morgan');
@@ -6,49 +5,75 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var session = require('express-session');
 var dotenv = require('dotenv');
+var passport = require('passport');
+var Auth0Strategy = require('passport-auth0');
 
-// We are using the dotenv library to load our environmental variables from the .env file.
 dotenv.load();
 
-// Import routes
 var routes = require('./routes/index');
 
-// Instantiates the Express JS framework
+// This will configure Passport to use Auth0
+var strategy = new Auth0Strategy({
+    domain:       process.env.AUTH0_DOMAIN,
+    clientID:     process.env.AUTH0_CLIENT_ID,
+    clientSecret: process.env.AUTH0_CLIENT_SECRET,
+    callbackURL:  process.env.AUTH0_CALLBACK_URL || 'http://localhost:3000/callback'
+  }, function(accessToken, refreshToken, extraParams, profile, done) {
+    // accessToken is the token to call Auth0 API (not needed in the most cases)
+    // extraParams.id_token has the JSON Web Token
+    // profile has all the information from the user
+    return done(null, profile);
+  });
+
+passport.use(strategy);
+
+// you can use this section to keep a smaller payload
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function(user, done) {
+  done(null, user);
+});
+
 var app = express();
 
-// The .set method allows us to configure various options with the Express framework. Here we are setting our views directory as well as telling Express that our templating engine will be Jade.
+// view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-// The .use method is similar to the .set method, where it allows us to set further configurations. The .use method also acts as a chain of events that will take place once a request hits our Node Js application. First we'll log the request data, parse any incoming data, and so on.
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(session({
-  // Here we are creating a unique session identifier
   secret: 'shhhhhhhhh',
-  resave: true, 
+  resave: true,
   saveUninitialized: true
 }));
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Here we are going to use add our routes in a use statement which will link the routes we defined to our app.
 app.use('/', routes);
 
-// catch 404 and forward to error handler.
-app.use(function(req, res, next){
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
-// If our applicatione encounters an error, we'll display the error and stacktrace accordingly.
-app.use(function(err, req, res, next){
+// error handlers
+
+app.use(function(err, req, res, next) {
   res.status(err.status || 500);
-  res.send(err);
+  res.render('error', {
+    message: err.message,
+    error: err
+  });
 });
 
-
-// Finally, we'll choose to have our app listen on port 3000. This means that once we launch our app, we'll be able to navigate to localhost:3000 and see our app in action. 
 app.listen(3000);
--- routes/index.js
+++ routes/index.js
@@ -1,27 +1,49 @@
-// Importing libraries to be used 
 var express = require('express');
+var passport = require('passport');
 var router = express.Router();
+var ensureLoggedIn = require('connect-ensure-login').ensureLoggedIn();
+var request = require('request');
 
-// On our router variable, we'll be able to include various methods. For our app we'll only make use of GET requests, so the method router.get will handle that interaction. This method takes a string as its first parameter and that is the url path, so for the first route we are just giving it '/', which means the default route. Next we are defining a Node Js callback function, that takes three parameters, a request (req), a response (res), and an optional next (next) parameter. Finally, in our callback function, we are just send the message "You are on the homepage".
-router.get('/', function(req, res, next){
-  res.send('You are on the homepage');
+var env = {
+  AUTH0_CLIENT_ID: process.env.AUTH0_CLIENT_ID,
+  AUTH0_DOMAIN: process.env.AUTH0_DOMAIN,
+  AUTH0_CALLBACK_URL: process.env.AUTH0_CALLBACK_URL || 'http://localhost:3000/callback'
+};
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { env: env });
 });
 
-router.get('/login', function(req, res){
-  res.send('You are on the login page');
-});
+router.get('/login',
+  function(req, res){
+    res.render('login', { env: env });
+  });
 
 router.get('/logout', function(req, res){
-  res.send('You are on the logout page');
+  req.logout();
+  res.redirect('/');
 });
 
-router.get('/polls', function(req, res){
-  res.send('You are on the polls page');
+router.get('/polls', ensureLoggedIn, function(req, res){
+  request('http://elections.huffingtonpost.com/pollster/api/charts.json?topic=2016-president', function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      var polls = JSON.parse(body);
+      res.render('polls', {user: req.user, polls: polls});
+    } else {
+      res.render('error');
+    }
+  })
+})
+
+router.get('/user', ensureLoggedIn, function(req, res, next) {
+  res.render('user', { user: req.user });
 });
 
-router.get('/users', function(req, res, next){
-  res.send('You are on the users page');
-});
+router.get('/callback',
+  passport.authenticate('auth0', { failureRedirect: '/url-if-something-fails' }),
+  function(req, res) {
+    res.redirect(req.session.returnTo || '/polls');
+  });
 
-// Finally, we export this module so that we can import it in our app.js file and gain access to the routes we defined.
 module.exports = router;
